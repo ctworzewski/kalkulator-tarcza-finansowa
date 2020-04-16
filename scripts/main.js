e3210422f54d81c4e2a616ae6ec0aaf566a877c7
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2020-04-16 22:53:54 +0200

    Create set button and name functions

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+let button = document.querySelector('.button');
+// let btn = document.addEventListener('click')
+
+button.addEventListener('click', function () {
+    console.log('Przycisk działa');
+})
